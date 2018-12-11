71256424167c4271a3ca91836f56de72739c4d12
Maciejf85 <Fialek85@gmail.com>
2018-12-11 09:07:54 +0100

    webpack-config v0.1

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import '../scss/main.scss';
+
+console.log('Hello webpack');
+const z = [1,2,3].map(n => n**2);
+console.log(z);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
-const path = require('path');
+const {resolve} = require('path');
 
 module.exports = {
-  entry: './src/home.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
-  }
-};
+
+     entry: './src/js/app.js',
+
+     mode: 'development',
+
+     output: {
+          filename: 'bundle.js',
+          path: resolve(__dirname, "dist")
+     },
+
+     module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
+                }
+              }
+            },
+            {
+                 test: /\.scss$/,
+               use: [
+                   "style-loader", // creates style nodes from JS strings
+                   "css-loader", // translates CSS into CommonJS
+                   "sass-loader" // compiles Sass to CSS, using Node Sass by default
+               ]},
+               {
+                    test: /\.(png|jpg|gif)$/i,
+                    use: [
+                      {
+                        loader: 'url-loader',
+                        options: {
+                          limit: 16384 // 16Kb
+                        }
+                      }
+                    ]
+               }
+          ]
+        }
+}
