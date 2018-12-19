b3baeb55da75c9ac5917eeeff610ddf63c0dd76f
Maciejf85 <Fialek85@gmail.com>
2018-12-19 13:16:10 +0100

    GameReady

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 
-
-<img alt="Logo" src="http://maciejf.pl/img/Furry-main.png" width="100%">
-
-
-
-
-<img alt="Logo" src="http://maciejf.pl/img/board1.jpg" width="25%"><img alt="Logo" src="http://maciejf.pl/img/board2.jpg" width="25%"><img alt="Logo" src="http://maciejf.pl/img/board3.jpg" width="25%"><img alt="Logo" src="http://maciejf.pl/img/board4.jpg" width="25%">
-
-
 # Furry Game
 ### http://maciejf.pl/furry-game/dist/
 
-## Przygotowanie
+## Opis
 
-> [...]
-> - ```[...]```
-> - [...]
-> - [...]
 
+> - Gra napisana obiektowo według wytycznych warsztatu.
+> - Ruch postaci osiągnięty przy pomocy sprites image. 
 <img alt="Logo" src="http://maciejf.pl/img/girl.png" height="120px">
-
+> - Gra umożliwia wybór poziomu trudności polegający na szybszym poruszaniu się postaci.
+> - Przy pomocy zapytań ajax i prostego skryptu w PHP gra zapisuje na serwerze najlepszy wynik.
+> - Każdorazowo losowany jest diament w jednej z pięciu grafik.
 <img alt="Logo" src="http://maciejf.pl/img/diamonds2.png">
+
+
+## Zasady gry
+
+> - Gra zaczyna się od tablicy startowej na której należy wybrać poziom trudności.
+> - Postacią kieruje się przy pomocy strzałem na klawiaturze.
+> - Gra kończy się wraz z wykryciem kolizji ze ścianą.
+> - Ilość punktów za zdobycie diamentu przyznawana jest w zależności od wybranego poziomu trudności:
+   > - easy - 1pkt
+   > - medium - 3pkt
+   > - hard - 7pkt
+>
+
+
+
+
+
+
+
+<img alt="Logo" src="http://maciejf.pl/img/Furry-main.png" width="100%">
+<img alt="Logo" src="http://maciejf.pl/img/board1.jpg" width="25%"><img alt="Logo" src="http://maciejf.pl/img/board2.jpg" width="25%"><img alt="Logo" src="http://maciejf.pl/img/board3.jpg" width="25%"><img alt="Logo" src="http://maciejf.pl/img/board4.jpg" width="25%">
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,12 @@
 import '../scss/main.scss';
 
-var arr = ["url(../src/img/diamond-1.png)" , 
-           "url(../src/img/diamond-2.png)" , 
-           "url(../src/img/diamond-3.png)" , 
-           "url(../src/img/diamond-4.png)" ,
-           "url(../src/img/diamond-5.png)"
-          ];
+var arr = ["url(../src/img/diamond-1.png)",
+     "url(../src/img/diamond-2.png)",
+     "url(../src/img/diamond-3.png)",
+     "url(../src/img/diamond-4.png)",
+     "url(../src/img/diamond-5.png)"
+];
 
-          
 
 var bestPoints = 0;
 var bestPointsEnd = document.querySelector('.end-bestScore');
@@ -15,114 +14,116 @@ var starterBoard = document.querySelector('.startBoard');
 var endBoard = document.querySelector('.endBoard');
 
 var character = document.createElement('div');
-     character.classList.add('girl');
+character.classList.add('girl');
 
 var diamondEl = document.createElement('div');
-     diamondEl.classList.add('diamond');
+diamondEl.classList.add('diamond');
 
 var replay = document.querySelector('.endBoard-replay');
-     replay.addEventListener('click', startBoard);
+replay.addEventListener('click', startBoard);
 
 var hardBtn = document.querySelectorAll('[class*=Btn]');
-     hardBtn.forEach(function(item){
-          item.addEventListener('click',function(){
-               var level = this.classList.value;
-               var scoreD = 0;
-               if(level == "easyBtn") {
-                    level = 500;
-                    character.style.animationDuration = '1s';
-                    scoreD = 1;
-               }
-               else if(level == "mediumBtn"){
-                    level = 200;
-                    character.style.animationDuration = '0.5s';
-                    scoreD = 3;
-               }
-               if(level == "hardBtn"){
-                    level = 70;
-                    character.style.animationDuration = '0.25s';
-                    scoreD = 7;
-               }
-              startGame(level, scoreD);
-          })
+hardBtn.forEach(function (item) {
+     item.addEventListener('click', function () {
+          var level = this.classList.value;
+          var scoreD = 0;
+          if (level == "easyBtn") {
+               level = 500;
+               character.style.animationDuration = '1s';
+               scoreD = 1;
+          } else if (level == "mediumBtn") {
+               level = 200;
+               character.style.animationDuration = '0.5s';
+               scoreD = 3;
+          }
+          if (level == "hardBtn") {
+               level = 70;
+               character.style.animationDuration = '0.25s';
+               scoreD = 7;
+          }
+          startGame(level, scoreD);
+     })
 })
 
-import { Character } from './char';
-import { Diamond } from './diam';
+import {
+     Character
+} from './char';
+import {
+     Diamond
+} from './diam';
 
 
-var Game = function() {
+var Game = function () {
 
      this.char = new Character(),
-     this.diamond = new Diamond(),
-     this.board = document.querySelectorAll('#board > div'),
-     this.scoreBoard = document.getElementById('points');
+          this.diamond = new Diamond(),
+          this.board = document.querySelectorAll('#board > div'),
+          this.scoreBoard = document.getElementById('points');
      this.mainBoard = document.getElementById('board');
      this.boardScreen = document.querySelector('.backgroundContainer');
      this.boardScore = document.querySelector('.score');
      this.endScore = document.querySelector('.end-score');
      var _this = this;
      this.score = 0,
-     this.scoreDiff = 1,
-     this.x = 0,
-     this.y = 0
+          this.scoreDiff = 1,
+          this.x = 0,
+          this.y = 0
 
-     document.addEventListener('keydown', function(event){
+     document.addEventListener('keydown', function (event) {
           _this.turnChar(event);
-      });
+     });
 
-     
-     this.position = function(x,y) {
+
+     this.position = function (x, y) {
           return x + (y * 10);
-     }     
+     }
 
-     this.showDiamond = function() {
-          var diamNumber = Math.round(Math.random() * (arr.length-1)) //losowanie diamentu
+     this.showDiamond = function () {
+          var diamNumber = Math.round(Math.random() * (arr.length - 1)) //losowanie diamentu
           diamondEl.style.backgroundImage = arr[diamNumber]; // przypisanie diamentu z tablicy do background
-          this.board[this.position(this.diamond.x , this.diamond.y)].appendChild(diamondEl); //
+          this.board[this.position(this.diamond.x, this.diamond.y)].appendChild(diamondEl); //
      }
 
-     this.showCharacter = function(){
-          this.board[this.position(this.char.x , this.char.y)].appendChild(character);
+     this.showCharacter = function () {
+          this.board[this.position(this.char.x, this.char.y)].appendChild(character);
      }
 
-     this.startTimer = function() {
-          this.id = setInterval(function(){
-               _this.moveChar();
-          }, this.char.speed)},
+     this.startTimer = function () {
+               this.id = setInterval(function () {
+                    _this.moveChar();
+               }, this.char.speed)
+          },
 
-     this.addPoint = function(){
-          this.score += this.scoreDiff;
-          this.scoreBoard.innerHTML = this.score;
-          this.diamond = new Diamond();
-          this.showDiamond();
-     }
-     this.clearClass = function(){
+          this.addPoint = function () {
+               this.score += this.scoreDiff;
+               this.scoreBoard.innerHTML = this.score;
+               this.diamond = new Diamond();
+               this.showDiamond();
+          }
+     this.clearClass = function () {
           character.classList.remove('left');
           character.classList.remove('right');
           character.classList.remove('up');
           character.classList.remove('down');
      }
-     this.gameOver = function(){
+     this.gameOver = function () {
           clearInterval(this.id);
           this.endScore.innerHTML = this.score;
           this.boardScreen.style.display = 'none';
           this.boardScore.style.display = 'none';
           endBoard.style.display = 'block';
 
-          if(this.score <= bestPoints){
-               console.log('gameOver => this.score <= bestScore '+ this.score , bestPoints)
+          if (this.score <= bestPoints) {
                bestPointsRead();
                bestPointsEnd.innerHTML = bestPoints;
                endBoard.classList.remove('win');
-          }else{
-               endBoard.classList.add('win');
+          } else {
+               newRecord(this.score);
                bestPointsEnd.innerHTML = this.score;
-               newRecord(this.score);             
-               console.log('gameOver => this.score > bestScore '+ this.score , bestPoints)
+               endBoard.classList.add('win');
           }
      }
-     this.startGame = function(level, scoreD){
+     this.startGame = function (level, scoreD) {
           this.scoreDiff = scoreD;
           this.char.speed = level;
           this.scoreBoard.innerHTML = this.score;
@@ -130,143 +131,143 @@ var Game = function() {
           this.boardScreen.style.display = 'block';
           this.boardScore.style.display = 'block';
           endBoard.style.display = 'none';
-          starterBoard.style.display = 'none';         
+          starterBoard.style.display = 'none';
      }
 
-     this.moveChar = function(){
+     this.moveChar = function () {
 
 
-          if((this.char.x >= 0 && this.char.x <= 9)  && (this.char.y >= 0 && this.char.y <= 9)){
+          if ((this.char.x >= 0 && this.char.x <= 9) && (this.char.y >= 0 && this.char.y <= 9)) {
 
-               if(this.char.direction == "right"){
+               if (this.char.direction == "right") {
                     this.x++;
 
-                    if(this.x >= 10){
+                    if (this.x >= 10) {
                          this.gameOver();
-                    }else{
+                    } else {
                          this.char.x = this.x;
                          this.clearClass();
-                         character.classList.add('right');   
+                         character.classList.add('right');
                     }
                }
 
-               if(this.char.direction == "down"){
+               if (this.char.direction == "down") {
                     this.y++;
 
-                    if(this.y >= 10){
+                    if (this.y >= 10) {
                          this.gameOver();
-                    }else{
+                    } else {
                          this.char.y = this.y;
                          this.clearClass();
-                         character.classList.add('down');   
+                         character.classList.add('down');
                     }
                }
 
-               if(this.char.direction == "left"){
+               if (this.char.direction == "left") {
                     this.x--;
 
-                    if(this.x < 0){
+                    if (this.x < 0) {
                          this.gameOver();
-                    }else{
+                    } else {
                          this.char.x = this.x;
                          this.clearClass();
-                         character.classList.add('left');   
+                         character.classList.add('left');
                     }
                }
 
-               if(this.char.direction == "up"){
+               if (this.char.direction == "up") {
                     this.y--;
 
-                    if(this.y < 0){
+                    if (this.y < 0) {
                          this.gameOver();
-                    }else{
+                    } else {
                          this.char.y = this.y;
                          this.clearClass();
-                         character.classList.add('up');   
+                         character.classList.add('up');
                     }
                }
 
-              
 
 
-               if(this.char.x == this.diamond.x && this.char.y == this.diamond.y){
+
+               if (this.char.x == this.diamond.x && this.char.y == this.diamond.y) {
                     this.addPoint();
                }
                this.showCharacter();
-
           }
 
 
 
      }
-          this.turnChar = function(event){
+     this.turnChar = function (event) {
 
-               switch (event.which) {
-                    case 37:
-                      this.char.direction = 'left';
-                      break;
-     
-                      case 38:
-                      this.char.direction = 'up';
-                      break;
-     
-                      case 39:
-                      this.char.direction = 'right';
-                      break;
-     
-                      case 40:
-                      this.char.direction = 'down';
-                      break;
-     
-               }
+          switch (event.which) {
+               case 37:
+                    this.char.direction = 'left';
+                    break;
+
+               case 38:
+                    this.char.direction = 'up';
+                    break;
+
+               case 39:
+                    this.char.direction = 'right';
+                    break;
+
+               case 40:
+                    this.char.direction = 'down';
+                    break;
+
           }
-          
+     }
+
 }
 
 
 
-function startBoard(){
-     starterBoard.style.display = "block";  
-     endBoard.style.display = 'none';   
+function startBoard() {
+     starterBoard.style.display = "block";
+     endBoard.style.display = 'none';
 }
 
-function startGame(level, scoreD){
+function startGame(level, scoreD) {
      var game = new Game();
 
-     game.startGame(level,scoreD);     
+     game.startGame(level, scoreD);
      game.showCharacter();
      game.showDiamond();
      game.startTimer();
 }
 
-startBoard();
 
-function newRecord(result){
+function newRecord(result) {
 
      var score = {
           points: result
      }
 
-$.ajax({ //Wysłanie nowego wyniku
+     $.ajax({ //Wysłanie nowego wyniku
           type: "POST", //typ połączenia na post
           url: "../points/points.php",
           data: {
-          score: score
-               },
-     }).done(function(){
+               score: score
+          },
+     }).done(function () {
           bestPointsRead();
-     }).fail(function(error){
-               console.log('error' + error)
-          });
+     }).fail(function (error) {
+          console.log('error' + error)
+     });
 
 }
-function bestPointsRead(){
-          $.ajax({
-               url: "../points/points.php"
-          }).done(function(response){
-               bestPoints = response.points;
-     }).fail(function(error){
+
+function bestPointsRead() {
+     $.ajax({
+          url: "../points/points.php"
+     }).done(function (response) {
+          bestPoints = response.points;
+     }).fail(function (error) {
           console.log('error' + error)
      })
 }
+startBoard();
 bestPointsRead();
